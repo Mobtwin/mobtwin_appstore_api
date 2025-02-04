5ec58f62ad0e4c8e377b42b3aca9c8790965cff3
Bourichi-Taha <bourichi.taha@gmail.com>
2025-02-04 09:51:10 +0100

    max restart 2 g

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
         instances: "1", // Number of instances (e.g., 1, 2, or 'max' for clustering mode)
         exec_mode: "cluster", // Use 'cluster' for multiple instances or 'fork' for a single instance
         watch: true, // Restart app on file changes
-        max_memory_restart: '1G', // Restart the app if memory usage exceeds this value
+        max_memory_restart: '2G', // Restart the app if memory usage exceeds this value
       },
     ],
   };
